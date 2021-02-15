3f91fc89e1ae40a2da1dee63d7a24b91d0e7403d
Jacob Sussmilch <jacobsussmilch187@gmail.com>
2021-02-15 18:07:50 +1100

    Updated API to hold correct ABS data. Fixed error html

--- flaskapp/api/resources.py
+++ flaskapp/api/resources.py
@@ -8,8 +8,11 @@ class WelcomeAPI(Resource):
         ret = ({
                 "message": "Welcome to the 'Learning & Development In Children - Data Analysis' API.",
                 "routes": [
-                    {"/api/g44" : "Census 2016, G44 Labour force status by sex of parents by age of dependent children for Couple families (SA2+)"},
-                    {"/api/g45" : "Census 2016, G45 Labour force status by sex of parent by age of dependent children for one parent families (SA2+)"},
+                    {"/api/ABS" : "Our polished ABS dataset"},
+                    {"/api/ABS/g44A" : "Census 2016, G44 Labour force status by sex of parents by age of dependent children for Couple families (SA2+)"},
+                    {"/api/ABS/g44A_descriptors" : "Descriptors for Census 2016, G44 Labour force status by sex of parents by age of dependent children for Couple families (SA2+)"},
+                    {"/api/ABS/g45A" : "Census 2016, G45 Labour force status by sex of parent by age of dependent children for one parent families (SA2+)"},
+                    {"/api/ABS/g45B" : "Census 2016, G45 Labour force status by sex of parent by age of dependent children for one parent families (SA2+)"},
                     {"/api/AEDC/communication" : "Census 2016, G45 Labour force status by sex of parent by age of dependent children for one parent families (SA2+)"},
                     {"/api/AEDC/social" : "Census 2016, G45 Labour force status by sex of parent by age of dependent children for one parent families (SA2+)"},
                     {"/api/AEDC/health" : "Census 2016, G45 Labour force status by sex of parent by age of dependent children for one parent families (SA2+)"},
@@ -20,10 +23,14 @@ class WelcomeAPI(Resource):
                     ]}, 200)
         return ret[0], ret[1]
 
+# ABS
+ABS_df = json.loads(pd.read_csv('data/ABS/clean_2016_abs.csv', delimiter=',', header=0, index_col=False).to_json(orient="records"))
+g44A_descriptors_df = json.loads(pd.read_csv('data/ABS/2016Census_G44A_cell_descriptors.csv', delimiter=',', header=0, index_col=False).to_json(orient="records"))
+g44A_df = json.loads(pd.read_csv('data/ABS/2016Census_G44A_VIC_SA2.csv', delimiter=',', header=0, index_col=False).to_json(orient="records"))
+g45A_df = json.loads(pd.read_csv('data/ABS/2016Census_G45A_VIC_SA2.csv', delimiter=',', header=0, index_col=False).to_json(orient="records"))
+g45B_df = json.loads(pd.read_csv('data/ABS/2016Census_G45B_VIC_SA2.csv', delimiter=',', header=0, index_col=False).to_json(orient="records"))
 
-g44_df = json.loads(pd.read_csv('data/g44.csv', delimiter=',', header=0, index_col=False).to_json(orient="records"))
-g45_df = json.loads(pd.read_csv('data/g45.csv', delimiter=',', header=0, index_col=False).to_json(orient="records"))
-
+#AEDC
 AEDC_social_df = json.loads(pd.read_csv('data/AEDC/social.csv', delimiter=',', header=0, index_col=False).to_json(orient="records"))
 AEDC_communication_df = json.loads(pd.read_csv('data/AEDC/communication.csv', delimiter=',', header=0, index_col=False).to_json(orient="records"))
 AEDC_health_df = json.loads(pd.read_csv('data/AEDC/health.csv', delimiter=',', header=0, index_col=False).to_json(orient="records"))
@@ -32,16 +39,34 @@ AEDC_language_df = json.loads(pd.read_csv('data/AEDC/language.csv', delimiter=',
 AEDC_oneormore_df = json.loads(pd.read_csv('data/AEDC/oneormore.csv', delimiter=',', header=0, index_col=False).to_json(orient="records"))
 AEDC_twoormore_df = json.loads(pd.read_csv('data/AEDC/twoormore.csv', delimiter=',', header=0, index_col=False).to_json(orient="records"))
 
-class ABS_G44_API(Resource):
+class ABS_API(Resource):
 
     def get(self):
-        ret = (g44_df, 200)
+        ret = (ABS_df, 200)
         return ret[0], ret[1]
 
-class ABS_G45_API(Resource):
+class ABS_G44A_API(Resource):
 
     def get(self):
-        ret = (g45_df, 200)
+        ret = (g44A_df, 200)
+        return ret[0], ret[1]
+
+class ABS_G44A_DESCRIPTORS_API(Resource):
+
+    def get(self):
+        ret = (g44A_descriptors_df, 200)
+        return ret[0], ret[1]
+
+class ABS_G45A_API(Resource):
+
+    def get(self):
+        ret = (g45A_df, 200)
+        return ret[0], ret[1]
+
+class ABS_G45B_API(Resource):
+
+    def get(self):
+        ret = (g45B_df, 200)
         return ret[0], ret[1]
 
 class AEDC_SOCIAL_API(Resource):
--- flaskapp/api/routes.py
+++ flaskapp/api/routes.py
@@ -9,8 +9,11 @@ api_bp = Blueprint('api', __name__)
 # Intializing API routes function used when intializing the app
 def initialise_api_routes(api):
     api.add_resource(WelcomeAPI, '/api')
-    api.add_resource(ABS_G44_API, '/api/g44')
-    api.add_resource(ABS_G45_API, '/api/g45')
+    api.add_resource(ABS_API, '/api/ABS')
+    api.add_resource(ABS_G44A_DESCRIPTORS_API, '/api/g44A_descriptors')
+    api.add_resource(ABS_G44A_API, '/api/g44A')
+    api.add_resource(ABS_G45A_API, '/api/g45A')
+    api.add_resource(ABS_G45B_API, '/api/g45B')
     api.add_resource(AEDC_SOCIAL_API, '/api/AEDC/social')
     api.add_resource(AEDC_HEALTH_API, '/api/AEDC/health')
     api.add_resource(AEDC_EMOTIONAL_API, '/api/AEDC/emotional')
